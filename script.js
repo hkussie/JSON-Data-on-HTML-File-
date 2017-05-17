f52a6e22ad7818c01a11f4833654b0e3598fe620
Harrison Kussie <hkussie@gmail.com>
2017-05-17 00:29:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var xmlhttp = new XMLHttpRequest();
+//var xmlhttp = new XMLHttpRequest();
 
-xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		data = JSON.parse(this.responseText);
-		document.getElementById("content").innerHTML = data.interests[1];
-	}
-};
+//xmlhttp.onreadystatechange = function() {
+//	if (this.readyState == 4 && this.status == 200) {
+//		data = JSON.parse(this.responseText);
+//		document.getElementById("content").innerHTML = data.interests[1];
+//	}
+//};
 
-xmlhttp.open("GET", "people.json", true);
-xmlhttp.send();
+//xmlhttp.open("GET", "people.json", true);
+//xmlhttp.send();
+
+//!-->
+
+//Use JQuery to get data from json file when user clicks button 
+$("button").click( function() {
+ $.getJSON( "people.json", function(obj) {
+  $.each(obj, function(key, value) {
+         $("ul").append("<li>"+value.name+"'s age is : "+value.age+"</li>");
+  });
+ });
+});
